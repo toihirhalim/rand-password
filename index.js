147422739a2b87d95d06d3f0cf5af2a88e4337b7
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-09-20 16:46:52 +0100

    synchronous callback

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const defaultOptions = {
     maxLength: 16
 }
 
-function generatePassword(options) {
+function generatePassword(options, callback) {
     option = { ...defaultOptions, ...options }
 
     let length = Math.floor(Math.random() * (option.maxLength - option.minLength)) + option.minLength;
@@ -13,17 +13,15 @@ function generatePassword(options) {
     for (let i = 0; i < length; i++)
         password += String.fromCharCode(Math.floor(Math.random() * (126 - 32)) + 32);
 
+    
+    if (callback && typeof callback == 'function') 
+        callback(null, password)
+
     return password
 }
 
 async function generatePasswordAsync(options, callback) {
-    const password = generatePassword(options)
-
-    if (callback && typeof callback == 'function') {
-        callback(null, password)
-    }
-
-    return password
+    return generatePassword(options, callback)
 }
 
 module.exports = { generatePassword, generatePasswordAsync }
